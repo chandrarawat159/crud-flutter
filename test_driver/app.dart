cafc683a95e35f78c1c661b6eee808dce90c4ad1
Aishwarya Shrestha <aish.stha07@gmail.com>
2024-07-14 13:25:53 +0545

    int

--- test_driver/app.dart
+++ test_driver/app.dart
@@ -2,6 +2,7 @@ import 'package:flutter_driver/driver_extension.dart';
 import 'package:flutter_crud/main.dart' as app;
 
 void main() {
+  Future.delayed(Duration(seconds: 3));
   enableFlutterDriverExtension();
   app.main();
 }
